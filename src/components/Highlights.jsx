e9009e1862b56e08c4f78421a25a50b75db62b66
Satrio Wicaksono <satwica69@gmail.com>
2024-06-19 08:32:15 +0700

    feat: Add model section 3D iPhone 15 Pro

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -25,9 +25,9 @@ const Highlights = () => {
     >
       <div className="screen-max-width">
         <div className="mb-12 w-full md:flex items-end justify-between">
-          <h1 id="title" className="section-heading">
+          <h2 id="title" className="section-heading">
             Get the highlights.
-          </h1>
+          </h2>
 
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
